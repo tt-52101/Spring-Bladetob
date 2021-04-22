a10c924d8f636bed4aff5ed0cfe60758f4186360
bingo <bingo@jinghang.com>
2021-04-22 14:42:05 +0800

    更新controller

--- blade-service/sf-resource/src/main/java/cn/rzedu/sf/resource/controller/MediaResourceController.java
+++ blade-service/sf-resource/src/main/java/cn/rzedu/sf/resource/controller/MediaResourceController.java
@@ -109,7 +109,6 @@ public class MediaResourceController extends BladeController {
 		@ApiParam(value = "一级分类")@RequestParam(value = "sortId",required = false) Integer sortId,
 		@ApiParam(value = "subject 71=软笔 72=硬笔",required = true)@RequestParam(value = "subject") Integer subject,
 		@ApiParam(value = "title")@RequestParam(value = "title",required = false) String title) {
-
 			IPage<MediaResourceVO> pages = mediaResourceService.selectMediaResourceSortSearch(Condition.getPage(query),mediaType,sortId,subject,title);
 			return R.data(pages);
 		}
@@ -135,7 +134,6 @@ public class MediaResourceController extends BladeController {
 		if (!title.isEmpty() && !title.equals(" ")){
 			mediaResourceService.saveSearchHistory(title,userId,username,subject);
 		}
-
 		IPage<MediaResourceVO> pages = mediaResourceService.selectMediaResourceQuery(Condition.getPage(query),subject,title);
 		return R.data(pages);
 	}
